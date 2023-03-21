4bd93d5edb1b91f30f12c440d61f1a0fe84a8415
BigBoiMan123 <gandshuang@gmail.com>
2023-03-20 17:29:54 -0700

    added comments, invalid operation detection, cleaned some stuff up

--- calculator.cpp
+++ calculator.cpp
@@ -1,46 +1,76 @@
+/* a very simple calculator written in C++
+   licensed under the GNU GPL V3 license
+   Copyright 2023 Steven Huang */
+
+// libraries and what not
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
+// introduce user
 int intro() {
     cout << "This is a basic calculator written in C++. \n";
     return 0;
     }
 
+// interactively collect operation and number from user and process it
 int input() {
+
+    // declare variables
     float x;
     int y;
-    char operation;
-    cout << "What operation would you like to perform? (a for addition, s for subtraction, m for multiplication, d for division.) \n";
-    cin >> operation;
+    string operationuser;
+    int realoperation;
+
+    // ask for the operation and process it
+    cout << "What operation would you like to perform? (addition, subtraction, multiplication, division) \n";
+    cin >> operationuser;
+    if (operationuser == "addition") {
+        realoperation = 1;
+    }
+    if (operationuser == "subtraction") {
+        realoperation = 2;
+    }
+    if (operationuser == "multiplication") {
+        realoperation = 3;
+    }
+    if (operationuser == "division") {
+        realoperation = 4;
+    }
+    if (operationuser != "division" && operationuser != "multiplication" && operationuser != "subtraction" && operationuser != "addition") {
+        cout << "ERROR: Invalid operation. \n";
+        return 0;
+    }
+    // ask for the numbers and write them into variables x and y
     cout << "Type the first number. \n";
     cin >> x;
     cout << "Type the second number. \n";
     cin >> y;
+
+    // calculate result and output it
     cout << "Calculating! \n";
-    switch(operation) {
-        case 'a':
+    switch (realoperation) {
+        case 1:
             cout << x + y;
             break;
-        case 's':
+        case 2:
             cout << x - y;
             break;
-        case 'm':
+        case 3:
             cout << x * y;
             break;
-        {
-            case 'd':
+        {case 4:
             double dresult = x / y;
             cout << setprecision(4) << dresult;
-            break;
-        }
+            break;}
         default:
-            cout << "ERROR: Invalid operation specified.";
-            break;
+            cout << "ERROR: An unknown error occured.";
         }
     return 0;
     }
 
+// run the damn thing
 int main() {
     intro();
     input();
